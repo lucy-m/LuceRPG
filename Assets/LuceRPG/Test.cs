ab016cd933b2e9dda78a86e8938d632d25565e32
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-01-18 15:13:51 +0000

    Create project, import assets

---
+++ Assets/LuceRPG/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSharp;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Say.hello("Hi");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
