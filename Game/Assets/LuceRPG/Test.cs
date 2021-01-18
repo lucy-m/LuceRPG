baf54d9c98a34a0e3a1d379e7ec65b778bc9b671
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-01-18 16:54:34 +0000

    Create server and shared solutions

--- Game/Assets/LuceRPG/Test.cs
+++ Game/Assets/LuceRPG/Test.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FSNetStandard20Lib;
+using UnityEngine.Networking;
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log("Starting");
-        Debug.Log(Say.aString);
+        Debug.Log("Text from FS lib: " + Say.aString);
+
+        StartCoroutine(GetText());
+    }
+
+    private IEnumerator GetText()
+    {
+        Debug.Log("Sending web request");
+        var webRequest = UnityWebRequest.Get("https://localhost:5001/WeatherForecast/helloWorld");
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.isNetworkError || webRequest.isHttpError)
+        {
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            var text = webRequest.downloadHandler.text;
+            Debug.Log("Text from server: " + text);
+        }
     }
 
     // Update is called once per frame
