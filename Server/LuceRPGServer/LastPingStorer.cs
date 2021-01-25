7402355456a9f9a0e6b3589227c2a6051802e9ae
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-01-25 11:26:46 +0000

    Create a LastPingStorer

---
+++ Server/LuceRPGServer/LastPingStorer.cs
@@ -0,0 +1,33 @@
+﻿using LuceRPG.Models;
+using LuceRPG.Server.Core;
+using Microsoft.FSharp.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace LuceRPG.Server
+{
+    public class LastPingStorer
+    {
+        private FSharpMap<string, long> _store;
+
+        public LastPingStorer()
+        {
+            _store = new FSharpMap<string, long>(Array.Empty<Tuple<string, long>>());
+        }
+
+        public void Update(string clientId, long timestamp)
+        {
+            _store = _store.Add(clientId, timestamp);
+        }
+
+        public IEnumerable<WithId.Model<IntentionModule.Payload>> Cull()
+        {
+            var staleThreshold = TimestampProvider.Now - 10_000L;
+
+            var culled = LastPingStore.cull(staleThreshold, _store);
+            _store = culled.updated;
+
+            return culled.intentions;
+        }
+    }
+}
