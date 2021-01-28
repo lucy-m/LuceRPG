c3fa580dff8dd5d46d14eda558c52488e37d8841
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-01-28 13:29:50 +0000

    Add copy script

---
+++ copy-dlls.bat
@@ -0,0 +1,3 @@
+copy "D:\Dev\Unity\LuceRPG\Shared\LuceRPG.Serialisation\bin\Release\netstandard2.0\LuceRPG.Serialisation.dll" "D:\Dev\Unity\LuceRPG\Game\Assets\Resources\DLLs"
+copy "D:\Dev\Unity\LuceRPG\Shared\LuceRPG.Models\bin\Release\netstandard2.0\LuceRPG.Models.dll" "D:\Dev\Unity\LuceRPG\Game\Assets\Resources\DLLs"
+copy "D:\Dev\Unity\LuceRPG\Shared\LuceRPG.Extensions\bin\Release\netstandard2.0\LuceRPG.Utility.dll" "D:\Dev\Unity\LuceRPG\Game\Assets\Resources\DLLs"
