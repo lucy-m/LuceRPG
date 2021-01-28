050cb8b8694b385b1de1bf38ea05df0669216d42
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-01-28 15:23:04 +0000

    Add IncorrectCredentials result to GetJoinGameResult

--- Shared/LuceRPG.Models/GetJoinGameResult.fs
+++ Shared/LuceRPG.Models/GetJoinGameResult.fs
@@ -4,5 +4,6 @@ module GetJoinGameResult =
     type Model =
         | Success of Id.Client * Id.WorldObject * World WithTimestamp
         | Failure of string
+        | IncorrectCredentials
 
 type GetJoinGameResult = GetJoinGameResult.Model
