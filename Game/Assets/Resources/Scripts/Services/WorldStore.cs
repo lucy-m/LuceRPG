dad8ceae2e79a9166d6a800033971937b312b1b7
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-02-02 20:14:40 +0000

    Add a welcome screen

---
+++ Game/Assets/Resources/Scripts/Services/WorldStore.cs
@@ -0,0 +1,16 @@
+﻿using LuceRPG.Models;
+
+public class WorldStore
+{
+    public WorldModule.Model World { get; private set; }
+
+    public void Apply(WorldEventModule.Model worldEvent)
+    {
+        World = EventApply.apply(worldEvent, World);
+    }
+
+    public bool HasWorld()
+    {
+        return World != null;
+    }
+}
