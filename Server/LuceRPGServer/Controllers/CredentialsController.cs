f3e6e5ec53a48a39eefdf8392c51132f29e3d583
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-02-03 18:26:42 +0000

    Allow credentials to be added dynamically (#19)

---
+++ Server/LuceRPGServer/Controllers/CredentialsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuceRPG.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CredentialsController : ControllerBase
+    {
+        private readonly ICredentialService _credentialService;
+        private readonly ILogger<CredentialsController> _logger;
+
+        public CredentialsController(
+            ICredentialService credentialService,
+            ILogger<CredentialsController> logger)
+        {
+            _credentialService = credentialService;
+            _logger = logger;
+        }
+
+        [HttpPut("addOrReset")]
+        public string AddOrReset(string username)
+        {
+            _logger.LogInformation($"Adding or resetting credentials for {username}");
+            return _credentialService.AddOrResetUser(username);
+        }
+    }
+}
--- Server/LuceRPGServer/CredentialService.cs
+++ Server/LuceRPGServer/CredentialService.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace LuceRPG.Server
 {
     public interface ICredentialService
     {
         bool IsValid(string username, string password);
+
+        string AddOrResetUser(string username);
     }
 
     public class CredentialService : ICredentialService
@@ -18,6 +22,7 @@ namespace LuceRPG.Server
             public string? Password { get; set; }
         }
 
+        private readonly string path = "credentials.json";
         private readonly ILogger<CredentialService> _logger;
         private readonly Dictionary<string, string> Credentials;
 
@@ -27,8 +32,7 @@ namespace LuceRPG.Server
 
             Credentials = new Dictionary<string, string>();
 
-            using var stream = new StreamReader("credentials.json");
-            var json = stream.ReadToEnd();
+            var json = File.ReadAllText(path);
             var creds = JsonConvert.DeserializeObject<List<Credential>>(json);
 
             foreach (var c in creds)
@@ -53,11 +57,40 @@ namespace LuceRPG.Server
                 return false;
             }
         }
+
+        public string AddOrResetUser(string username)
+        {
+            var password = Guid.NewGuid().ToString();
+            Credentials[username] = password;
+
+            SaveCredentials();
+
+            return password;
+        }
+
+        private void SaveCredentials()
+        {
+            var credentialList = Credentials.Select(kvp =>
+                new Credential
+                {
+                    Username = kvp.Key,
+                    Password = kvp.Value
+                }
+            );
+            var json = JsonConvert.SerializeObject(credentialList, Formatting.Indented);
+
+            File.WriteAllText(path, json);
+        }
     }
 
     public class TestCredentialService : ICredentialService
     {
         public bool IsValidReturn = false;
+
+        public string AddOrResetUser(string username)
+        {
+            return "";
+        }
 
         public bool IsValid(string username, string password)
         {
