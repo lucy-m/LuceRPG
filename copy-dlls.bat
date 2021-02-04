5cf4cdf3e0a6edeb066898d019c6fe01a5e4e097
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-02-04 11:13:17 +0000

    Update copy-dlls.bat

--- copy-dlls.bat
+++ copy-dlls.bat
@@ -1,3 +1,3 @@
-copy "D:\Dev\Unity\LuceRPG\Shared\LuceRPG.Serialisation\bin\Release\netstandard2.0\LuceRPG.Serialisation.dll" "D:\Dev\Unity\LuceRPG\Game\Assets\Resources\DLLs"
-copy "D:\Dev\Unity\LuceRPG\Shared\LuceRPG.Models\bin\Release\netstandard2.0\LuceRPG.Models.dll" "D:\Dev\Unity\LuceRPG\Game\Assets\Resources\DLLs"
-copy "D:\Dev\Unity\LuceRPG\Shared\LuceRPG.Extensions\bin\Release\netstandard2.0\LuceRPG.Utility.dll" "D:\Dev\Unity\LuceRPG\Game\Assets\Resources\DLLs"
+copy "Shared\LuceRPG.Serialisation\bin\Release\netstandard2.0\LuceRPG.Serialisation.dll" "Game\Assets\Resources\DLLs"
+copy "Shared\LuceRPG.Models\bin\Release\netstandard2.0\LuceRPG.Models.dll" "Game\Assets\Resources\DLLs"
+copy "Shared\LuceRPG.Extensions\bin\Release\netstandard2.0\LuceRPG.Utility.dll" "Game\Assets\Resources\DLLs"
