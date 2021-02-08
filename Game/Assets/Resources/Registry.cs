170f78c1d58115d14b1d41428c9d37343c82aacf
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-02-08 12:02:28 +0000

    Add registry

---
+++ Game/Assets/Resources/Registry.cs
@@ -0,0 +1,14 @@
+﻿using LuceRPG.Game.Stores;
+
+namespace LuceRPG.Game
+{
+    public static class Registry
+    {
+        public static StoreRegistry Stores { get; private set; } = new StoreRegistry();
+
+        public static void Reset()
+        {
+            Stores = new StoreRegistry();
+        }
+    }
+}
---
+++ Game/Assets/Resources/Stores/StoreRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuceRPG.Game.Stores
+{
+    public class StoreRegistry
+    {
+        public WorldStore World { get; private set; } = new WorldStore();
+        public ConfigStore Config { get; private set; } = new ConfigStore();
+    }
+}
