19f23c04ba56bc88461f865535fc0f6c2deff7fe
lucy-m <39138024+lucy-m@users.noreply.github.com>
2021-02-12 18:51:53 +0000

    Add server side data to intention processing

---
+++ Shared/LuceRPG.Models/ServerSideData.fs
@@ -0,0 +1,28 @@
+﻿namespace LuceRPG.Models
+
+module ServerSideData =
+    type ObjectClientMap = Map<Id.WorldObject, Id.Client>
+    type UsernameClientMap = Map<string, Id.Client>
+
+    type Model =
+        {
+            objectClientMap: ObjectClientMap
+            usernameClientMap: UsernameClientMap
+        }
+
+    let create
+            (objectClientMap: ObjectClientMap)
+            (usernameClientMap: UsernameClientMap)
+            : Model =
+        {
+            objectClientMap = objectClientMap
+            usernameClientMap = usernameClientMap
+        }
+
+    let empty: Model =
+        {
+            objectClientMap = Map.empty
+            usernameClientMap = Map.empty
+        }
+
+type ServerSideData = ServerSideData.Model
