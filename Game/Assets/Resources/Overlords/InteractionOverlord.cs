4c14bf0d18883d7eba5a855d4d6243b5f2ba4231
lucy-m <39138024+lucy-m@users.noreply.github.com>
2022-01-23 13:37:32 +0000

    Remove previous SpeechBubbles on interact

--- Game/Assets/Resources/Overlords/InteractionOverlord.cs
+++ Game/Assets/Resources/Overlords/InteractionOverlord.cs
@@ -1,6 +1,7 @@
 ﻿using LuceRPG.Game.WorldObjects;
 using LuceRPG.Models;
 using Microsoft.FSharp.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace LuceRPG.Game.Overlords
@@ -9,6 +10,8 @@ namespace LuceRPG.Game.Overlords
     {
         public GameObject SpeechBubblePrefab = null;
         public Canvas WorldTextCanvas = null;
+
+        private IList<SpeechBubbleController> previousSpeechBubbles = null;
 
         private void Start()
         {
@@ -19,12 +22,27 @@ namespace LuceRPG.Game.Overlords
         {
             Debug.Log($"Running interaction {interaction.id}");
 
+            if (previousSpeechBubbles != null)
+            {
+                foreach (var c in previousSpeechBubbles)
+                {
+                    if (c != null)
+                    {
+                        Destroy(c.gameObject);
+
+                    }
+                }
+            }
+
+            previousSpeechBubbles = new List<SpeechBubbleController>();
+
             foreach (var iOne in interaction.value)
             {
                 var text = iOne.Item;
                 var speechBubble = Instantiate(SpeechBubblePrefab, position, Quaternion.identity, WorldTextCanvas.transform);
                 var sbc = speechBubble.GetComponent<SpeechBubbleController>();
                 sbc.ShowText(text);
+                previousSpeechBubbles.Add(sbc);
             }
         }
     }
